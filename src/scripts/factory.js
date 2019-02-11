e9c59fe5059b572b32d48976ec4fbdf8ee18bca2
Panya Farnette <naienko@gmail.com>
2019-02-11 09:39:06 -0600

    changed null return to useful data

--- src/scripts/factory.js
+++ src/scripts/factory.js
@@ -1,10 +1,14 @@
 // makes each job's card
 
 const jobFactory = jobElement => {
+    let end_year = jobElement.end_year;
+    if (end_year === null) {
+        end_year = "now";
+    }
     return `<section>
     <h3>${jobElement.organization}</h3>
     <div>${jobElement.city}, ${jobElement.state}</div>
-    <div>${jobElement.start_year} to ${jobElement.end_year}</div>
+    <div>${jobElement.start_year} to ${end_year}</div>
     <div>${jobElement.role}</div>
     </section>`;
 }
